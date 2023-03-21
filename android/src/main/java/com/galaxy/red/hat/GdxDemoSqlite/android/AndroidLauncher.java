f333d608ca1c7c490ee619221dcabaf074d3c01f
joonthansyle <joonthansyle@redhatlenovo.ph>
2023-03-22 03:08:15 +0800

    First Commit

---
+++ android/src/main/java/com/galaxy/red/hat/GdxDemoSqlite/android/AndroidLauncher.java
@@ -0,0 +1,17 @@
+package com.galaxy.red.hat.GdxDemoSqlite.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.galaxy.red.hat.GdxDemoSqlite.AppMain;
+
+/** Launches the Android application. */
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration configuration = new AndroidApplicationConfiguration();
+		initialize(new AppMain(), configuration);
+	}
+}
