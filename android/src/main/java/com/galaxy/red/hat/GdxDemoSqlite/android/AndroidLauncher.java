51a55362019e5af863f85fe57cf66d4a2ce6a767
joonthansyle <joonthansyle@redhatlenovo.ph>
2023-03-22 22:30:52 +0800

    First Commit after Clean

---
+++ android/src/main/java/com/galaxy/red/hat/GdxDemoSqlite/android/AndroidLauncher.java
@@ -0,0 +1,18 @@
+package com.galaxy.red.hat.GdxDemoSqlite.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.galaxy.red.hat.GdxDemoSqlite.AppMain;
+
+/** Launches the Android application. */
+public class AndroidLauncher extends AndroidApplication {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        AndroidApplicationConfiguration configuration = new AndroidApplicationConfiguration();
+        configuration.useImmersiveMode = true; // Recommended, but not required.
+        initialize(new AppMain(), configuration);
+    }
+}
